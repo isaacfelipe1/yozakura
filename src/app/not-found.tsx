33b100e595deb01b6e296116bf7225139d90b955
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-07-19 11:13:18 -0300

    feat: include img

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,25 @@
-import Link from 'next/link';
-import Footer from '../../components/footer';
+import Link from 'next/link'
+import Footer from '../../components/footer'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h2 className="text-4xl font-bold text-gray-800">Página não existe</h2>
-      <p className="text-lg mt-4 text-gray-700">A página que você está procurando não foi encontrada.</p>
-      <Link href="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
-          Voltar para Home
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <img
+        src="/404.jpg"
+        alt="Página não encontrada"
+        className="w-64 h-64 mb-6"
+      />
+      <h2 className="text-4xl font-bold text-red-600">Página não existe</h2>
+      <p className="text-lg mt-4 text-gray-700">
+        A página que você está procurando não foi encontrada.
+      </p>
+      <Link
+        href="/"
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300"
+      >
+        Voltar para Home
       </Link>
       <Footer />
     </div>
-  );
+  )
 }
