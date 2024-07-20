dc524fb3a52abca777d861e8c680ac2867d67e0f
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-07-19 23:47:44 -0300

    feat: udpade

--- components/UserPosts.tsx
+++ components/UserPosts.tsx
@@ -186,7 +186,7 @@ const UserPosts = () => {
                 onClick={handleLoadMore}
                 className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Carregar mais Posts
+                Carregar mais...
               </button>
             </div>
           )}
