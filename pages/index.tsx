16d842c9ffd5dc267923387b9cb2062e5a3e3699
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-07-27 02:47:45 -0300

    feat: add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import Posts from '../components/Posts'
 import Footer from '../components/footer'
 import './../src/app/globals.css'
@@ -28,6 +29,16 @@ const HomePage: React.FC<HomePageProps> = ({ isLoggedIn }) => {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <Head>
+        <title>Blog Yozakura</title>
+        <meta name="description" content="Bem vindo ao blog Yozakura" />
+        <meta name="keywords" content="blog, yozakura, nextjs" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Blog Yozakura" />
+        <meta property="og:description" content="Bem vindo ao blog Yozakura" />
+        <meta name="robots" content="index, follow, nocache" />
+        <meta name="googlebot" content="index, follow" />
+      </Head>
       {isLoggedIn ? <ClientNavbar /> : <Navbar isLoggedIn={isLoggedIn} />}
       <div className="flex-grow container mx-auto p-4">
         <VLibras forceOnload />
