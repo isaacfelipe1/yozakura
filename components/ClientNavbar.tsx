30174cd81c5fa1770bdcfa9c1c4ee9b0d7f8fe57
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-07-29 22:30:16 -0300

    feat: remove button logout icone

--- components/ClientNavbar.tsx
+++ components/ClientNavbar.tsx
@@ -70,7 +70,8 @@ const ClientNavbar: React.FC = () => {
               onClick={handleLogout}
               className="text-white flex items-center cursor-pointer"
             >
-              <TbLogout className="text-2xl hover:text-[#F2E981]" />
+              {/* <TbLogout className="text-2xl hover:text-[#F2E981]" /> */}
+              <span className=" hover:text-[#F2E981]">Sair</span>
             </div>
           </div>
         </div>
